58c3f57cf7cb38c3a89af2196ef66c70e5523828
jane-muthoka <janemuthoka5@gmail.com>
2025-10-15 10:00:00 +0300

    new files

---
+++ cookies/index.js
@@ -0,0 +1,3 @@
+accept-cookies=document.getElementById('accept-cookies');
+decline-cookies=document.getElementById('decline-cookies');
+cookie-notification=document.getElementById('cookie-notification');
