84550d7cb015c649c343e2ae92b188288ff8781c
jane-muthoka <janemuthoka5@gmail.com>
2025-10-17 09:00:10 +0300

    script file

--- cookies/index.js
+++ cookies/index.js
@@ -1,3 +1,19 @@
-accept-cookies=document.getElementById('accept-cookies');
-decline-cookies=document.getElementById('decline-cookies');
-cookie-notification=document.getElementById('cookie-notification');
+accept_cookies=document.getElementById('accept-cookies');
+decline_cookies=document.getElementById('decline-cookies');
+cookie_notification=document.getElementById('cookie-notification');
+
+accept_cookies.addEventListener('click',function(){
+    document.cookie="cookies_accepted=true; path=/; max-age="+60*60*24*365;
+    cookie_notification.style.display="none";
+});
+
+decline_cookies.addEventListener('click',function(){
+    document.cookie="cookies_accepted=false; path=/; max-age="+60*60*24*365;
+    cookie_notification.style.display="none";
+});
+if(document.cookie.indexOf('cookies_accepted=')===-1){
+    cookie_notification.style.display="block";
+}   
+else{
+    cookie_notification.style.display="none";
+}
