0ed712fc51c6c6469c3d27ed0e1cd5b43c5c63fd
jane-muthoka <janemuthoka5@gmail.com>
2025-12-31 10:44:08 +0300

    changes

--- cookies/index.js
+++ cookies/index.js
@@ -1,16 +1,21 @@
 accept_cookies=document.getElementById('accept-cookies');
 decline_cookies=document.getElementById('decline-cookies');
 cookie_notification=document.getElementById('cookie-notification');
+text=document.getElementById('txt');
 
 accept_cookies.addEventListener('click',function(){
     document.cookie="cookies_accepted=true; path=/; max-age="+60*60*24*365;
-    cookie_notification.style.display="none";
+    cookie_notification.style.display="block";
+    accept_cookies.style.display="none";
+    decline_cookies.style.display="none";
+    text.style.display="none";
 });
 
 decline_cookies.addEventListener('click',function(){
     document.cookie="cookies_accepted=false; path=/; max-age="+60*60*24*365;
     cookie_notification.style.display="none";
 });
+
 if(document.cookie.indexOf('cookies_accepted=')===-1){
     cookie_notification.style.display="block";
 }   
